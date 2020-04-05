8792c13976fd8fc5340542a96bd34375f2dede7f
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-04-05 17:57:13 -0400

    Commit inicial

---
+++ product.py
@@ -0,0 +1,19 @@
+"""Modelo para Productos."""
+
+products = [
+{
+'name':'laptop',
+'price':8000,
+'cantidad':4
+},
+{
+'name':'mouse',
+'price':40,
+'cantidad':10
+},
+{
+'name':'monitor',
+'price':400,
+'cantidad':3
+}
+]
